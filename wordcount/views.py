d8da07d72e81c8863dbb920c131138c3609e36c1
Rob Daly <rjd@mobulz.com>
2018-10-25 17:12:53 -0400

    Initial working site

---
+++ wordcount/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def earlyhomepage(request):
+    return render(request, "earlyhome.html", {"Hithere" : "This is me"})
+
+def joblistings(request):
+    return HttpResponse("<h1>Here are your job listings</h1>")
+
+def homepage(request):
+    return render(request, "home.html")
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+
+    word_dictionary = {}
+
+    for word in wordlist:
+        if word in word_dictionary:
+            #increase
+            word_dictionary[word] += 1
+        else:
+            # add to word_dictionary
+            word_dictionary[word] = 1
+
+    sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request,"count.html", {'fulltext':fulltext, 'count': len(wordlist), 'sorted_words': sorted_words})
+
+def about(request):
+    return render(request,"about.html")
